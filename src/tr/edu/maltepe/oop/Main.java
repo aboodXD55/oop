051a7617d3e63d3160feae1a088a3551842ccde7
abood <https://github.com/aboodXD55/hazim-is-king.git>
2022-03-15 14:30:12 +0300

    first lab

--- src/tr/edu/maltepe/oop/Main.java
+++ src/tr/edu/maltepe/oop/Main.java
@@ -1,20 +1,29 @@
 package tr.edu.maltepe.oop;
 
-public class Main {
+public class Main{
 
-    public static void main(String[] args) {
-        student student1= new student();
-        student1.setstudentname("hadi");
-        student1.setstudentage(23);
+    public static void main(String [] er)
+    {
 
-        prof prof1= new prof();
-        prof1.setprofname("ensar");
-        prof1.setprofsalery(2000);
+        Student Student1= new Student();
+        Student1.setName("hadi");
+        Student1.setAge(String.valueOf(23));
+        Student1.setstu_ID(55);
+        Student1.studying();
 
-        System.out.println("student name is "+student1.getstudentname());
-        System.out.println("student age is "+student1.getstudentage());
-        System.out.println("prof name is "+prof1.getprofname());
-        System.out.println("prof salery is "+prof1.getprofsalery());
+        Prof prof1= new Prof();
+        prof1.setName("ensar");
+        prof1.setAge(String.valueOf(50));
+        prof1.setPro_ID(33);
+        prof1.teaches();
 
+        System.out.println("Student Name: " + Student1.getName());
+        System.out.println("Age of Student is:"+Student1.getAge());
+        System.out.println("Student ID: "+Student1.getstu_ID());
+
+
+        System.out.println("Prof Name: " + prof1.getName());
+        System.out.println("Age of Prof is:"+prof1.getAge());
+        System.out.println("Prof ID: "+prof1.getpro_ID(13));
     }
 }
